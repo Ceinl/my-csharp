adaa13f869a8581673d98dc5d0f800bfd6b9822d
Dima Slyva <slyva.dima@gmail.com>
2023-08-02 14:26:32 +0200

    Program writed.

--- L01/ConsoleApp1/Program.cs
+++ L01/ConsoleApp1/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace ConsoleApp1
+﻿/*
+     
+ 
+ 
+*/
+
+namespace ConsoleApp1
 {
     internal class Program
     {
+
+        static void sum(int first_number, int second_number) {Console.Write($"Answer: {first_number + second_number} \n");}
+
+        static void dec(int first_number, int second_number) { Console.Write($"Answer:{first_number - second_number} \n"); }
+
+        static void mult(int first_number, int second_number) { Console.Write($"Answer:{first_number * second_number} \n"); }
+
+        static void div(int first_number, int second_number) { Console.Write($"Answer:{first_number / second_number} \n"); }
+
+        static (int, int) GetNumbers()
+        {
+            Console.WriteLine("Please enter the first number:");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number:");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            return (firstNumber, secondNumber);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            (var firstNumber, var secondNumber) = GetNumbers();
+
+            Console.WriteLine("Please chose action \n1 - Sum\n2-dec\n3-mult\n4-div");
+
+            int switchNum = int.Parse(Console.ReadLine());
+
+            switch (switchNum)
+            {
+                case 1: sum(firstNumber, secondNumber); break;
+                case 2: dec(firstNumber, secondNumber); break;
+                case 3: mult(firstNumber, secondNumber); break;
+                case 4: div(firstNumber, secondNumber); break;
+
+            }
+
+
+
         }
     }
 }
