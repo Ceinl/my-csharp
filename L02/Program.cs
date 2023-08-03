3f538f7ba6bae29abb57627aafb6e629588c33ca
Dima Slyva <slyva.dima@gmail.com>
2023-08-03 13:03:30 +0200

    L02 added

---
+++ L02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace L02
+{
+
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Animal(string name, string description) 
+        {
+            this.Name = name;
+            this.Description = description;
+
+        }
+        public void printName() { Console.WriteLine(Name); }
+        public void printDescription() { Console.WriteLine(Description); }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal("Bluse","White cat");
+            Animal dog = new Animal("jaz", "Dachshund");
+
+            dog.printName();
+
+            cat.printDescription();
+          
+
+            
+        }
+    }
+}
