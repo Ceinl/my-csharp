e68b2fb8d15fa2dcc01f77aeaac534ef3d6fa175
Dima Slyva <slyva.dima@gmail.com>
2023-08-03 13:12:14 +0200

    L03 added

---
+++ L03/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("give a number: ");
+        string userInput = Console.ReadLine();
+        int num;
+
+        try
+        {
+            num = Convert.ToInt32(userInput);
+            Console.WriteLine("resutf of divide by zero: " + (num / 0));
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: you cant divide by zero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: give a number please.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unknown error: " + ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
